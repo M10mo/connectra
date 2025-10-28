3921750b7a9c4d320000d41e41cf0c61ad0fcada
M10mo <mohdkanoongo@gmail.com>
2025-10-28 16:26:31 -0400

    built out sign up form and implemented link to login for existing account

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ShipWheelIcon } from "lucide-react"
+import { Link } from 'react-router';
 
 const SignUpPage = () => {
   const [signupData, setSignupData] = useState({
     fullName: "",
-    email:"",
-    password:"",
+    email: "",
+    password: "",
   });
 
   const handleSignup = (e) => {
@@ -12,9 +14,96 @@ const SignUpPage = () => {
   }
   return (
     <div className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8" data-theme="forest">
-      <div className='border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden '>
-      </div>
 
+      <div className='border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden '>
+        {/* signup for left hand side */}
+
+        <div className='w-full lg:w-1/2 p-4 sm:p-8 flex flex-col'>
+          {/*logo*/}
+          <div className='mb-4 flex items-center justify-start gap-2'>
+            <ShipWheelIcon className='size-9 text-primary' />
+            <span className='text-3xl fond-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secdonary tracking-wider'>
+              Connectra
+            </span>
+          </div>
+
+          <div className='w-full '>
+            <form onSubmit={handleSignup}>
+              <div className='space-y-4'>
+                <div>
+                  <h2 className='text-xl font-semibold'>Create an Account
+                  </h2>
+                  <p>Join Connectra and start your language learning journey now!</p>
+                </div>
+                {/*FULL NAME */}
+                <div className='space-y-3'>
+                  <div className='form-control w-full'>
+                    <label className='label'>
+                      <span className='label-text'>Full Name</span>
+                    </label>
+                    <input type='text'
+                      placeholder='Your Name'
+                      className='input input-bordered w-full'
+                      value={signupData.fullName}
+                      onChange={(e) => setSignupData({ ...signupData, fullName: e.target.value })}
+                      required />
+                  </div>
+                  {/*EMAIL*/}
+                  <div className='form-control w-full'>
+                    <label className='label'>
+                      <span className='label-text'>Email</span>
+                    </label>
+                    <input type='email'
+                      placeholder=''
+                      className='input input-bordered w-full'
+                      value={signupData.email}
+                      onChange={(e) => setSignupData({ ...signupData, email: e.target.value })}
+                      required />
+                  </div>
+                  {/*PASSWORD */}
+                  <div className='form-control w-full'>
+                    <label className='label'>
+                      <span className='label-text'>Password</span>
+                    </label>
+                    <input type='password'
+                      placeholder=''
+                      className='input input-bordered w-full'
+                      value={signupData.password}
+                      onChange={(e) => setSignupData({ ...signupData, password: e.target.value })}
+                      required />
+                    <p>Password must be at least 8 characters long!
+                    </p>
+                  </div>
+
+                  <div className="form-control">
+                    <label className="label cursor-pointer justify-start gap-2">
+                      <input type="checkbox" className="checkbox checkbox-sm" required />
+                      <span className="text-xs leading-tight">
+                        I agree to the{" "}
+                        <span className="text-primary hover:underline">terms of service</span> and{" "}
+                        <span className="text-primary hover:underline">privacy policy</span>
+                      </span>
+                    </label>
+                  </div>
+                </div>
+
+                <button className='btn btn-primary w-full' type='submit' >
+                  Create Account
+                </button>
+
+                <div className='text-center mt-4'>
+                  <p className='text-sm'>Already have an account?{" "}
+                    <Link to="/login" className='text-primary hover:underline'>
+                      Sign In
+                    </Link>
+                  </p>
+
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
 
     </div>
   )
