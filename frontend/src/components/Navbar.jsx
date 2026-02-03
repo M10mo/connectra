4e6000f6add0f43f4cea6d588b7fbae4c0558d6e
M10mo <mohdkanoongo@gmail.com>
2026-02-03 16:18:16 -0500

    working on building out navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { Link, useLocation } from "react-router";
+import useAuthUser from "../hooks/useAuthUser";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { logout } from "../lib/api";
+import { ShipWheelIcon } from "lucide-react";
 
 const Navbar = () => {
-    return (
-        <div>Navbar</div>
-    )
-}
+    const { authUser } = useAuthUser();
+    const location = useLocation();
+    const isChatPage = location.pathname?.startsWith("/chat");
 
-export default Navbar
+    const queryClient = useQueryClient()
+    const { mutate: logoutMutation } = useMutation({
+        mutationFn: logout,
+        onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] })
+    })
+
+    return <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex items-center justify-end w-full">
+                {isChatPage && (
+                    <div className="pl-5">
+                        <Link to='/' className="flex items-center gap-2.5">
+                            <ShipWheelIcon className="size-9 text-primary" />
+                            <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">Connectra
+                            </span>
+                        </Link>
+                    </div>
+                )}
+
+            </div>
+        </div>
+    </nav>
+};
+
+export default Navbar;
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import useAuthUser from "../hooks/useAuthUser";
 import { Link, useLocation } from "react-router"
-import { ShipWheelIcon } from "lucide-react";
+import { BellIcon, HomeIcon, ShipWheelIcon, UsersIcon } from "lucide-react";
 
 const Sidebar = () => {
     const { authUser } = useAuthUser();
@@ -18,6 +18,40 @@ const Sidebar = () => {
                         Connectra
                     </span>
                 </Link>
+            </div>
+
+            <nav className="flex-1 p-4 space-y-1">
+                <Link to="/" className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${currentPath === "/" ? "btn-active" : ""}`}>
+                    <HomeIcon className="size-5 text-base-content opacity-70" />
+                    <span>Home</span>
+                </Link>
+
+                <Link to='/friends' className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${currentPath === "/friends" ? "btn-active" : ""}`}>
+                    <UsersIcon className="size-5 text-base-content opacity-70" />
+                    <span>Friends</span>
+                </Link>
+
+                <Link to='/notifications' className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${currentPath === '/notifications' ? "btn-active" : ""}`}>
+                    <BellIcon className="size-5 text-base-content opacity-70" />
+                    <span>Notifications</span>
+                </Link>
+            </nav>
+
+            {/* user profile */}
+            <div className="p-4 border-t border-base-300 mt-auto">
+                <div className="flex items-center gap-3">
+                    <div className="avatar">
+                        <div className="w-10 rounded-full">
+                            <img src={authUser?.profilePic} alt="User Avatar" />
+                        </div>
+                    </div>
+                    <div className="flex-1">
+                        <p className="font-semibold text-sm">{authUser?.fullName}</p>
+                        <p className="text-xs text-success flex items-center gap-1">
+                            <span className="size-2 rounded-full bg-success inline-block" />
+                        </p>
+                    </div>
+                </div>
             </div>
         </aside>
     );
